7cbbf8223a2313e1e52a54cd9ff624a95773af14
Vitor Morimitsu <morimitsuv@gmail.com>
2025-12-04 21:12:51 -0300

    geometria

---
+++ src/geometria.c
@@ -0,0 +1,18 @@
+#include "geometria.h"
+
+double distanciaEntrePontos(double x1, double y1, double x2, double y2){
+    double horizontal = fabs(x2-x1);
+    double potenciaX = pow(horizontal, 2);
+
+    double vertical = fabs(y2-y1);
+    double potenciaY = pow(vertical,2);
+
+    double distancia = sqrt(potenciaX + potenciaY);
+
+    return distancia;
+}
+
+double determinante(double x1, double y1, double x2, double y2, double x3, double y3){
+    double det = x1*y2 + y1*x3 + x2*y3 - (y2*x3 + x1*y3 + y1*x2);
+    return det;
+}
---
+++ src/geometria.h
@@ -0,0 +1,39 @@
+#ifndef GEOMETRIA_H
+#define GEOMETRIA_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "pacote.h"
+#include "vertice.h"
+
+/*
+    Arquivo .h responsável por realizar todas as geometrias necessárias no segundo trabalho de estrutura de dados.
+*/
+
+/*
+    Realiza o cálculo da distância entre dois pontos.
+*/
+double distanciaEntrePontos(double x1, double y1, double x2, double y2);
+
+/* 
+    Função responsável por realizar o cálculo do determinante.
+*/
+double determinante(double x1, double y1, double x2, double y2, double x3, double y3);
+
+/*
+    Função responsável por calcular o ponto de intersecção de um segmento com uma reta de origem na bomba e destino no segmento
+    criará um novo vertice do polígono
+*/
+Vertice calculaIntersecçao(double xOrigem, double yOrigem, double xVertice, double yVertice);
+
+
+
+
+
+
+
+
+
+
+#endif
